14d4d67c4a6cc28d670c183f25033576f07e7789
locona <locona.0810@gmail.com>
2020-04-21 11:21:53 +0900

    fix: fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ cd "$GITHUB_WORKSPACE" || exit 1
 
 export GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo "MYSQL_HOST: ${MYSQL_HOST}"
-echo "MYSQL_PORT: ${MYSQL_PORT}"
-echo "MYSQL_USER: ${MYSQL_USER}"
-echo "MYSQL_PASSWORD: ${MYSQL_PASSWORD}"
-echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
-echo "SQLBOILER_OUTPUT: ${SQLBOILER_OUTPUT}"
+echo "MYSQL_HOST: ${INPUT_MYSQL_HOST}"
+echo "MYSQL_PORT: ${INPUT_MYSQL_PORT}"
+echo "MYSQL_USER: ${INPUT_MYSQL_USER}"
+echo "MYSQL_PASSWORD: ${INPUT_MYSQL_PASSWORD}"
+echo "MYSQL_DATABASE: ${INPUT_MYSQL_DATABASE}"
+echo "SQLBOILER_OUTPUT: ${INPUT_SQLBOILER_OUTPUT}"
 
-sed -i -e "/dbname:/s/dbname: .*$/dbname:  ${MYSQL_DATABASE}/" ${SQLBOILER_PATH}
-sed -i -e "/user:/s/user: .*$/user:  ${MYSQL_USER}/" ${SQLBOILER_PATH}
-sed -i -e "/pass:/s/pass: .*$/pass:  ${MYSQL_PASSWORD}/" ${SQLBOILER_PATH}
-sed -i -e "/port:/s/port: .*$/port:  ${MYSQL_PORT}/" ${SQLBOILER_PATH}
+sed -i -e "/dbname:/s/dbname: .*$/dbname:  ${INPUT_MYSQL_DATABASE}/" ${INPUT_SQLBOILER_PATH}
+sed -i -e "/user:/s/user: .*$/user:  ${INPUT_MYSQL_USER}/" ${INPUT_SQLBOILER_PATH}
+sed -i -e "/pass:/s/pass: .*$/pass:  ${INPUT_MYSQL_PASSWORD}/" ${INPUT_SQLBOILER_PATH}
+sed -i -e "/port:/s/port: .*$/port:  ${INPUT_MYSQL_PORT}/" ${INPUT_SQLBOILER_PATH}
 
 # ex) sqlboiler mysql --no-tests --no-hooks --wipe -c sqlboiler.yml -o models
-sqlboiler mysql --no-tests --no-hooks --wipe -c ${SQLBOILER_PATH} -o ${SQLBOILER_OUTPUT}
+sqlboiler mysql --no-tests --no-hooks --wipe -c ${INPUT_SQLBOILER_PATH} -o ${INPUT_SQLBOILER_OUTPUT}
